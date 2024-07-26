eb06db4ff24b0a21bb6fdfec7a9df18bca3bf23c
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-07-26 10:23:19 +0530

    crud_poc

--- task/src/main/java/com/trojan/task/service/ProjectService.java
+++
@@ -1,9 +0,0 @@
-package com.trojan.task.service;
-
-import javassist.NotFoundException;
-import com.trojan.task.entity.ProjectDetails;
-
-public interface ProjectService {
-
-    ProjectDetails getProjectById(Integer projectId) throws NotFoundException;
-}
